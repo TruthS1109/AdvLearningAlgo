57df0306885d70ba61fcf9111fe3dbf29bab5f2d
TruthS1109 <38018322+TruthS1109@users.noreply.github.com>
2022-06-30 15:43:07 -0500

    DecisionTrees.py
    Practice Lab exercise for Decision trees. C2_W4

---
+++ DecisionTrees.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from public_tests import *
+
+%matplotlib inline
+
+X_train = np.array([[1,1,1],[1,0,1],[1,0,0],[1,0,0],[1,1,1],[0,1,1],[0,0,0],[1,0,1],[0,1,0],[1,0,0]])
+y_train = np.array([1,1,0,0,1,0,0,1,1,0])
